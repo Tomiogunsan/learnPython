f5854b36a381139959e83fe03fbc481851c9bdaf
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-01-04 18:30:59 +0100

    implemented user model

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,7 @@ class CustomUserAdmin (UserAdmin):
         add_form = CustomUserCreationForm
         form = CustomUserChangeForm
         model = CustomUser
+        list_display = ['email', 'username', 'age', 'is_staff', ]
 
     
     
